ddf77c883fa9e3fd3939bf764ee3d0c93105059e
Akos-1 <veronicagyasi@gmail.com>
2024-02-18 12:26:21 -0800

    updated

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python3
+"""
+Define and annotate the following variables with the specified values
+a: An integer representing a value of 1.
+pi: A float representing the mathematical constant pi with a value of 3.14
+i_understand_annotations: A boolean indicating understanding of annotations,
+set to True
+school: A string representing the name of the school, which is "Holberton"
+"""
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
-
